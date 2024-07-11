a33701de1ff7ba21510f6726682f73757dfe1205
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-07-12 01:36:17 +0200

    added simple progressbar

--- Hasher/MainPage.xaml.cs
+++ Hasher/MainPage.xaml.cs
@@ -20,6 +20,7 @@ public partial class MainPage : ContentPage
 
 			// Create a hash from the file stream
 			var progress = new Progress<HashingProgress>();
+			progress.ProgressChanged += (sender, e) => ProgressBar.Progress = e.PercentageComplete;
 			var hash = await Hasher.HashService.MD5(fileStream, progress, CancellationToken.None);
 
 			// Display the hash
