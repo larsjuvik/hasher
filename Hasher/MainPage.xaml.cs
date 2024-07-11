d5fa809028133f60c32e549bda6c97c2858769b7
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-07-12 01:12:29 +0200

    added filepicker functionality

--- Hasher/MainPage.xaml.cs
+++ Hasher/MainPage.xaml.cs
@@ -2,23 +2,16 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async void SelectFileButton_Clicked(object sender, EventArgs e)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		// Open file picker
+		var filePickerResult = await FilePicker.Default.PickAsync(PickOptions.Default);
 	}
 }
 
