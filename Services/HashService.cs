65505b12f59fbd2a85a28f49fc31ccefd60c0907
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-07-11 21:35:50 +0200

    added some hashing methods

--- Services/HashService.cs
+++ Services/HashService.cs
@@ -1,6 +1,53 @@
 ﻿namespace Hashing;
 
-public class HashService
+public static class HashService
 {
+    public static readonly string[] HashAlgorithms = new string[]
+    {
+        "MD5",
+        "SHA1",
+        "SHA256",
+        "SHA384",
+        "SHA512"
+    };
 
+    public static string MD5(string input)
+    {
+        using var md5 = System.Security.Cryptography.MD5.Create();
+        var inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+        var hashBytes = md5.ComputeHash(inputBytes);
+        return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+    }
+
+    public static string SHA1(string input)
+    {
+        using var sha1 = System.Security.Cryptography.SHA1.Create();
+        var inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+        var hashBytes = sha1.ComputeHash(inputBytes);
+        return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+    }
+
+    public static string SHA256(string input)
+    {
+        using var sha256 = System.Security.Cryptography.SHA256.Create();
+        var inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+        var hashBytes = sha256.ComputeHash(inputBytes);
+        return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+    }
+
+    public static string SHA384(string input)
+    {
+        using var sha384 = System.Security.Cryptography.SHA384.Create();
+        var inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+        var hashBytes = sha384.ComputeHash(inputBytes);
+        return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+    }
+
+    public static string SHA512(string input)
+    {
+        using var sha512 = System.Security.Cryptography.SHA512.Create();
+        var inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+        var hashBytes = sha512.ComputeHash(inputBytes);
+        return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+    }
 }
