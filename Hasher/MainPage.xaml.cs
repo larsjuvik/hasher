c458abd7c29021e406e1fabc28ce8626db2d2b6b
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-07-12 01:25:15 +0200

    showing md5 hash of selected file (basic version)

--- Hasher/MainPage.xaml.cs
+++ Hasher/MainPage.xaml.cs
@@ -12,6 +12,23 @@ public partial class MainPage : ContentPage
 	{
 		// Open file picker
 		var filePickerResult = await FilePicker.Default.PickAsync(PickOptions.Default);
+
+		// Create a file stream from result
+		if (filePickerResult != null)
+		{
+			var fileStream = await filePickerResult.OpenReadAsync();
+
+			// Create a hash from the file stream
+			var progress = new Progress<HashingProgress>();
+			var hash = await Hasher.HashService.MD5(fileStream, progress, CancellationToken.None);
+
+			// Display the hash
+			HashLabel.Text = hash;
+		}
+		else
+		{
+			HashLabel.Text = "No file selected";
+		}
 	}
 }
 
