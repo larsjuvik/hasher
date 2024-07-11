003c0e2a6ca2b19d38a9419cfcc6104a6258c786
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-07-11 21:43:52 +0200

    added reference to hash service in tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,10 +1,14 @@
 namespace Tests;
 
-public class UnitTest1
+public class HashServiceTest
 {
+    // Test that the MD5 hash is correct
     [Fact]
-    public void Test1()
+    public void TestMD5()
     {
-
+        var input = "Hello, World!";
+        var expected = "65a8e27d8879283831b664bd8b7f0ad4";
+        var actual = Hashing.HashService.MD5(input);
+        Assert.Equal(expected, actual);
     }
 }
