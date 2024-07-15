f84d90d0cb5f4c029e3812bd660b35255d807e05
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-07-15 02:53:20 +0200

    WIP refactor to use viewmodels

---
+++ Hasher/Viewmodels/MainPageViewModel.cs
@@ -0,0 +1,15 @@
+namespace Hasher.Viewmodels;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+public partial class MainPageViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string selectedFileName;
+
+    [ObservableProperty]
+    private string hashAlgorithm;
+
+    [ObservableProperty]
+    private string hash;
+}
