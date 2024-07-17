98777a748a702f6e3e8eff855776f5a5221cc9e0
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-07-18 00:34:45 +0200

    removed ios/android/tizen targets

--- Hasher/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Hasher;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
