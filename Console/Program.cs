04ee865847508d163bec8a82c6b2feca35d402e9
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-08-12 17:14:56 +0200

    added command line parser

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CommandLine;
+
+var result = Parser.Default.ParseArguments<CommandLineOptions>(Environment.GetCommandLineArgs())
+    .WithNotParsed(errors =>
+    {
+        Console.WriteLine("Invalid command line arguments.");
+        Environment.Exit(1);
+    });
+
+class CommandLineOptions
+{
+    [Option('f', "file", Required = true, HelpText = "Path to the file to hash")]
+    public string? InputFile { get; set; }
+
+    [Option('a', "algorithm", Required = true, HelpText = "Hash algorithm to use")]
+    public string? Algorithm { get; set; }
+}
